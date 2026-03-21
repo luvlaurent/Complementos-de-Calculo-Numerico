7d0a37d23b35e9f3f88cdfd8da4816a891bd2553
rutepergentino <ras6@discente.ifpe.edu.br>
2026-03-21 09:32:57 -0300

    Refactor bisseccao function and add main for tests
    Updated bisseccao function to include default parameters and added a main function to run tests.

--- metodo-bissecao/questao11.py
+++ metodo-bissecao/questao11.py
@@ -1,7 +1,13 @@
+# Questão 11
+import numpy as np
+
 def f(x):
     return (x+2)*(x+1)*x*(x-1)**3*(x-2)
 
-def bisseccao(f, a, b, tol, max_iter):
+def bisseccao(f, a, b, tol=1e-5, max_iter=100):
+    if f(a)*f(b) >= 0:
+        return "Sem mudança de sinal"
+
     for i in range(max_iter):
         c = (a + b)/2
 
@@ -15,7 +21,13 @@ def bisseccao(f, a, b, tol, max_iter):
 
     return c
 
-print("a)", bisseccao(f, -3, 1, 1e-5, 100))
-print("b)", bisseccao(f, -2.5, 3, 1e-5, 100))
-print("c)", bisseccao(f, -1.75, 1.5, 1e-5, 100))
-print("d)", bisseccao(f, -1.5, 1.75, 1e-5, 100))
+def main():
+    print("QUESTÃO 11")
+
+    print("\n(a)", bisseccao(f, -3, 1))
+    print("(b)", bisseccao(f, -2.5, 3))
+    print("(c)", bisseccao(f, -1.75, 1.5))
+    print("(d)", bisseccao(f, -1.5, 1.75))
+
+if __name__ == "__main__":
+    main()
