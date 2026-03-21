b84eefc2f6279165d4e7be1a2f1def7123c9ea00
rutepergentino <ras6@discente.ifpe.edu.br>
2026-03-21 09:18:21 -0300

    Enhance bisseccao function and add plotting
    Updated the bisseccao function to include max_iter parameter and return iteration count. Added plotting functionality for visualizing the functions.

--- metodo-bissecao/questao10.py
+++ metodo-bissecao/questao10.py
@@ -1,22 +1,34 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def f(x):
     return x**2 - 1 - np.exp(1 - x**2)
 
-def bisseccao(f,a,b,tol=1e-3):
+def bisseccao(f, a, b, tol, max_iter):
+    for i in range(max_iter):
+        c = (a + b)/2
 
-    while (b-a)/2 > tol:
-
-        c = (a+b)/2
+        if abs(f(c)) < tol or (b - a)/2 < tol:
+            break
 
         if f(a)*f(c) < 0:
             b = c
         else:
             a = c
 
-    return (a+b)/2
+    return c, i+1
 
+raiz, it = bisseccao(f, -2, 0, 1e-3, 100)
 
-raiz = bisseccao(f,-2,0)
+print("Raiz:", raiz)
 
-print("Raiz:",raiz)
+# gráfico
+x = np.linspace(-2,2,1000)
+y1 = x**2 - 1
+y2 = np.exp(1 - x**2)
+
+plt.plot(x, y1, label="y = x^2 - 1")
+plt.plot(x, y2, label="y = e^(1-x^2)")
+plt.legend()
+plt.grid()
+plt.show()
