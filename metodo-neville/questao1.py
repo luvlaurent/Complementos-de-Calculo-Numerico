4adf7e38f0146b08f6cd9823941172bcdf8f201e
rutepergentino <ras6@discente.ifpe.edu.br>
2026-03-28 11:45:41 -0300

    Update questao1.py

--- metodo-neville/questao1.py
+++ metodo-neville/questao1.py
@@ -1,6 +1,8 @@
-import numpy as np
+# Questão 1 - Método de Neville
 
-# Método de Neville
+import numpy as np  # não é obrigatório, mas deixei pra padronizar
+
+# função do método de Neville
 def neville(xp, yp, x):
     n = len(xp)
     Q = [[0]*n for _ in range(n)]
@@ -18,24 +20,36 @@ def neville(xp, yp, x):
             i += 1
         j += 1
 
-    return Q[0][n-1]
+    return Q
+
+# função pra mostrar resultados por grau
+def resolver(xp, yp, x):
+    Q = neville(xp, yp, x)
+    print("Grau 1:", Q[0][1])
+    print("Grau 2:", Q[0][2])
+    print("Grau 3:", Q[0][3])
+    print()
 
 # a
-xp = [8.1,8.3,8.6,8.7]
-yp = [16.94410,17.56492,18.50515,18.82091]
-print("1a:", neville(xp, yp, 8.4))
+print("1a")
+resolver([8.1,8.3,8.6,8.7],
+         [16.94410,17.56492,18.50515,18.82091],
+         8.4)
 
 # b
-xp = [-0.75,-0.5,-0.25,0]
-yp = [-0.07181250,-0.02475000,0.33493750,1.10100000]
-print("1b:", neville(xp, yp, -1/3))
+print("1b")
+resolver([-0.75,-0.5,-0.25,0],
+         [-0.07181250,-0.02475000,0.33493750,1.10100000],
+         -1/3)
 
 # c
-xp = [0.1,0.2,0.3,0.4]
-yp = [0.62049958,-0.28398668,0.00660095,0.24842440]
-print("1c:", neville(xp, yp, 0.25))
+print("1c")
+resolver([0.1,0.2,0.3,0.4],
+         [0.62049958,-0.28398668,0.00660095,0.24842440],
+         0.25)
 
 # d
-xp = [0.6,0.7,0.8,1.0]
-yp = [-0.17694460,0.01375227,0.22363362,0.65809197]
-print("1d:", neville(xp, yp, 0.9))
+print("1d")
+resolver([0.6,0.7,0.8,1.0],
+         [-0.17694460,0.01375227,0.22363362,0.65809197],
+         0.9)
