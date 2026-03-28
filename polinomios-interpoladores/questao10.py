d2328d75c5d582d879de0aa2885b2e0621ed50d9
rutepergentino <ras6@discente.ifpe.edu.br>
2026-03-28 11:07:34 -0300

    Refactor polynomial interpolation function and variables

--- polinomios-interpoladores/questao10.py
+++ polinomios-interpoladores/questao10.py
@@ -1,33 +1,26 @@
-# Questão 10
-
 import sympy as sp
 
-x, x1 = sp.symbols('x x1')
+x1 = sp.Symbol('x1')
 
-def f(valor):
-    return sp.sqrt(valor - valor**2)
+def f(x):
+    return sp.sqrt(x - x**2)
 
-def P2(x_val):
-    pontos = [(0, f(0)), (x1, f(x1)), (1, f(1))]
-    P = 0
+def P(x):
+    xp = [0, x1, 1]
+    yp = [f(0), f(x1), f(1)]
 
+    s = 0
     i = 0
     while i < 3:
-        xi, yi = pontos[i]
-        termo = yi
+        p = yp[i]
         j = 0
         while j < 3:
             if i != j:
-                xj, _ = pontos[j]
-                termo *= (x_val - xj)/(xi - xj)
+                p *= (x - xp[j])/(xp[i] - xp[j])
             j += 1
-        P += termo
+        s += p
         i += 1
+    return s
 
-    return P
-
-eq = f(0.5) - P2(0.5) + 0.25
-
-sol = sp.solve(eq, x1)
-
-print("x1 =", sol)
+eq = f(0.5) - P(0.5) + 0.25
+print("x1 =", sp.solve(eq, x1))
