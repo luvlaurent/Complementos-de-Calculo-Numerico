ad1fb815dff89c4b044d3240099121fb0a345c57
rutepergentino <ras6@discente.ifpe.edu.br>
2026-03-28 11:10:06 -0300

    Refactor lagrange function and add new test cases

--- polinomios-interpoladores/questao8.py
+++ polinomios-interpoladores/questao8.py
@@ -1,38 +1,40 @@
-# Questão 8 
-
 import numpy as np
 
-def f(x):
-    return np.exp(x)
-
-def lagrange(x_pontos, y_pontos, x):
-    n = len(x_pontos)
+def lagrange(xp, yp, x):
+    n = len(xp)
     i = 0
-    resultado = 0
+    s = 0
 
     while i < n:
-        termo = y_pontos[i]
+        p = yp[i]
         j = 0
         while j < n:
             if i != j:
-                termo = termo * (x - x_pontos[j]) / (x_pontos[i] - x_pontos[j])
+                p *= (x - xp[j])/(xp[i] - xp[j])
             j += 1
-        resultado += termo
+        s += p
         i += 1
-
-    return resultado
-
-x_pontos = [0, 0.25, 0.5]
-y_pontos = [f(x_pontos[0]), f(x_pontos[1]), f(x_pontos[2])]
+    return s
 
 x = 0.43
+xp = [0, 0.25, 0.5]
 
-p1 = lagrange(x_pontos[:2], y_pontos[:2], x)
-p2 = lagrange(x_pontos, y_pontos, x)
+# a)
+def fa(x): return np.exp(x)
+yp = [fa(0), fa(0.25), fa(0.5)]
+print("8a:", abs(fa(x) - lagrange(xp, yp, x)))
 
-real = f(x)
+# b)
+def fb(x): return x**4 - x**3 + x**2 - x + 1
+yp = [fb(0), fb(0.25), fb(0.5)]
+print("8b:", abs(fb(x) - lagrange(xp, yp, x)))
 
-print("P1:", p1)
-print("P2:", p2)
-print("Erro P1:", abs(real - p1))
-print("Erro P2:", abs(real - p2))
+# c)
+def fc(x): return x**2*np.cos(x) - 3*x
+yp = [fc(0), fc(0.25), fc(0.5)]
+print("8c:", abs(fc(x) - lagrange(xp, yp, x)))
+
+# d)
+def fd(x): return np.log(np.exp(x)+2)
+yp = [fd(0), fd(0.25), fd(0.5)]
+print("8d:", abs(fd(x) - lagrange(xp, yp, x)))
