94ed19abe704df3da6793fe8df0d96bb6713260f
rutepergentino <ras6@discente.ifpe.edu.br>
2026-03-28 11:06:21 -0300

    Refactor Lagrange function variable names

--- polinomios-interpoladores/questao11.py
+++ polinomios-interpoladores/questao11.py
@@ -1,25 +1,21 @@
 # Questão 11
 
-x_pontos = [1.00, 1.05, 1.10, 1.15]
-y_pontos = [0.1924, 0.2414, 0.2933, 0.3492]
+xp = [1.00, 1.05, 1.10, 1.15]
+yp = [0.1924, 0.2414, 0.2933, 0.3492]
 
-def lagrange(x_pontos, y_pontos, x):
-    n = len(x_pontos)
+def lagrange(xp, yp, x):
+    n = len(xp)
     i = 0
-    resultado = 0
-
+    s = 0
     while i < n:
-        termo = y_pontos[i]
+        p = yp[i]
         j = 0
         while j < n:
             if i != j:
-                termo *= (x - x_pontos[j])/(x_pontos[i] - x_pontos[j])
+                p *= (x - xp[j])/(xp[i] - xp[j])
             j += 1
-        resultado += termo
+        s += p
         i += 1
+    return s
 
-    return resultado
-
-x = 1.09
-
-print("Aproximação:", lagrange(x_pontos, y_pontos, x))
+print("11:", lagrange(xp, yp, 1.09))
