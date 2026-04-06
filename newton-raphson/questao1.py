284cb9c08aca518c21944640601d87f98611b32b
rutepergentino <ras6@discente.ifpe.edu.br>
2026-04-06 13:09:40 -0300

    Refactor Newton's method implementation
    Refactor Newton's method to use a loop instead of a function.

--- newton-raphson/questao1.py
+++ newton-raphson/questao1.py
@@ -1,28 +1,26 @@
 import math
-
 # --------------------------------------------------
 # QUESTÃO 1
 # f(x) = x^2 - 6
 # p0 = 1
 # Calcular p2 usando Newton
 # --------------------------------------------------
+# Lista 3 - QUESTÃO 1 - Método de Newton
 
+# função f(x)
 def f(x):
     return x**2 - 6
 
+# derivada f'(x)
 def df(x):
     return 2*x
 
-def newton(p0, n_iter):
-    
-    p = p0
-    
-    for i in range(n_iter):
-        p = p - f(p)/df(p)
-        print(f"p{i+1} =",p)
-    
-    return p
+# valor inicial
+p = 1
 
+print("Iteração | Valor de p")
 
-# cálculo até p2
-newton(1,2)
+# vamos calcular até p2
+for i in range(2):
+    p = p - f(p)/df(p)
+    print(i+1, "      |", p)
